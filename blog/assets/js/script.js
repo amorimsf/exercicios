c2921b5f27ee8fc7139fdcbfde58610adbbcbb02
Matheus Amorim <136996208+amorimsf@users.noreply.github.com>
2024-03-13 22:21:22 -0300

    correção e commit final

--- blog/assets/js/script.js
+++ blog/assets/js/script.js
@@ -1,10 +1,9 @@
 setTimeout(() => {
-    const heartIcon = document.querySelectorAll('.icone-coracao');
+    const heartIcon = document.querySelectorAll('.heart-icon');
 
     heartIcon.forEach(function (icon) {
         icon.addEventListener('click', () => {
             icon.classList.toggle('active');
         });
     });
-}, 500);    
-    
+}, 500);
